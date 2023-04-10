1e2b5cba0ef24200609ca63555a6792b0b235d23
Jonas <jonas.koziorek@gmail.com>
2023-04-10 09:37:05 +0200

    Updated code

--- codes/1code.jl
+++ codes/1code.jl
@@ -6,18 +6,17 @@ dx = Systems.lorenz([-8.0, 8.0, 27.0]; σ = 10.0, ρ = 28.0, β = 8/3)
 
 time = 200.0
 step = 0.01
-xdat = trajectory(dx, time-step; Δt = step).data
+xdat,t = trajectory(dx, time-step; Δt = step)
 
-x = [a for (a,b,c) in xdat];
 function embed2(data, stackmax)
-    H = zeros(stackmax,length(x)-stackmax);
+    H = zeros(stackmax,length(data)-stackmax);
     for k = 1:stackmax
-        H[k,:] = x[k:end-stackmax-1+k]
+        H[k,:] = data[k:end-stackmax-1+k]
     end  
     return H
 end
 
 stackmax = 100
-H = embed2(x, stackmax);
+H = embed2(xdat[:,1], stackmax);
 
 U,S,V = svd(H)
